99626cb494f917acdb406ce39b85a78a4127bd2a
福士悠樹 <bica251112@bica.ac.jp>
2026-08-27 10:42:33 +0900

    その他ファイル分離

---
+++ lib/screens/class_otherscreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// ==========================================
+// その他画面
+// ==========================================
+
+class OtherScreen extends StatelessWidget {
+  const OtherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text('その他'),
+      ),
+      body: const Center(child: Text('その他画面', style: TextStyle(fontSize: 20))),
+    );
+  }
+}
